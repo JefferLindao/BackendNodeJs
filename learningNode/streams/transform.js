e1a1532441b252d6f9de833f3c92273db613ee40
JefferLindao <jefferlindao@gmail.com>
2020-06-06 14:58:30 -0500

    Reto camelCase

--- learningNode/streams/transform.js
+++ learningNode/streams/transform.js
@@ -1,8 +1,18 @@
 const { Transform } = require('stream')
 
+/**
+ * 
+ * @param {cadena} str transformamos una cadena de texto a camelCase
+ */
+function camelCase(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+    return index == 0 ? word.toLowerCase() : word.toUpperCase();
+  }).replace(/\s+/g, '');
+}
+
 const transformStream = new Transform({
   transform(chunk, encoding, callback) {
-    this.push(chunk.toString().toUpperCase())
+    this.push(camelCase(chunk.toString()))
     callback()
   }
 })
