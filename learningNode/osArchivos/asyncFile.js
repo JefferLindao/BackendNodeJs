0b60066fb21f5a1b380394858b21c921b36730b5
JefferLindao <jefferlindao@gmail.com>
2020-06-06 22:23:02 -0500

    OS y File

---
+++ learningNode/osArchivos/asyncFile.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const file = process.argv[2]
+
+if (!file) {
+  throw new Error('Debes Indicar el archivo que quieres leer')
+}
+
+const content = fs.readFile(file, (err, content) => {
+  if (err) {
+    console.log(err)
+  }
+  const lines = content.toString().split("\n").length
+  console.log(lines)
+})
---
+++ learningNode/osArchivos/os.js
@@ -0,0 +1,7 @@
+const os = require('os')
+// console.log('Cpu info', os.cpus())
+// console.log("IP address", os.networkInterfaces())
+// console.log("Free memory", os.freemem())
+// console.log('Type', os.type());
+// console.log('SP version', os.release())
+console.log('Usr info', os.userInfo())
