80a6fd862e23b21e3022b8a9035619c6e55b7427
JefferLindao <jefferlindao@gmail.com>
2020-06-16 14:00:02 -0500

    Middlerware Capas de manejo de errores

--- moviesApi/index.js
+++ moviesApi/index.js
@@ -3,9 +3,14 @@ const app = express()
 const { config } = require('./config/index')
 const moviesApi = require('./routes/movies.js')
 
+const { errorHandler, logErrors } = require('./utils/middleware/errorHandlers')
+
 app.use(express.json())
 
 moviesApi(app)
+
+app.use(logErrors)
+app.use(errorHandler)
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`)
